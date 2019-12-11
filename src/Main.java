fa3725b48a981e81d703cc17e5670731515ea96a
john <johnotters@yahoo.com>
2019-12-11 14:57:36 -0800

    Player object constructed with color arguments

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
+import Pieces.Piece;
+
 public class Main {
     public static void main(String[] args) {
-        ChessGame chessGame = new ChessGame(new HumanPlayer(), new HumanPlayer());
+        ChessGame chessGame = new ChessGame(new HumanPlayer(Piece.color.WHITE), new HumanPlayer(Piece.color.BLACK));
         chessGame.play();
 
-
-        Board board = new Board();
-
-        Display display = new CommandLineDisplay();
-
-        display.update(board);
     }
 }
