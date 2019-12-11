37ddd7104ee5247032fa88f5d795c511f442fe20
john <johnotters@yahoo.com>
2019-12-11 13:08:05 -0800

    Requires Display to handle all human interfacing.  This is so different display implementations can all work.

--- src/Display.java
+++ src/Display.java
@@ -1,5 +1,17 @@
+import Pieces.Piece;
+
+import java.util.ArrayList;
+
 public interface Display {
     void update(Board board);
 
     void displayMove(Move move);
+
+    void askForPieceToMove(Board board, Piece.color playerColor);
+
+    Position getSourcePiecePosition(Board board, Piece.color playerColor);
+
+    void askWhereToMove(Board board, Piece.color playerColor, ArrayList<Move> validMoves);
+
+    Position getDestinationPosition(Board board, Piece.color playerColor);
 }
