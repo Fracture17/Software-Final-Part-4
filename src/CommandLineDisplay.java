f9d5d6703b3fd39479e034003239ed8aae3e61cf
john <johnotters@yahoo.com>
2019-12-11 13:08:54 -0800

    Updates to satisfy new Display interface

--- src/CommandLineDisplay.java
+++ src/CommandLineDisplay.java
@@ -1,5 +1,8 @@
 import Pieces.Piece;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CommandLineDisplay implements Display {
     public void update(Board board) {
         for(int r = 0; r < board.getHeight(); r++) {
@@ -93,5 +96,41 @@ public class CommandLineDisplay implements Display {
         char destColChar = (char) (move.destCol + '1');
 
         System.out.print("" + sourceRowChar + sourceColChar + "->" + destRowChar + destColChar);
+    }
+
+    @Override
+    public void askForPieceToMove(Board board, Piece.color playerColor) {
+        System.out.println("Enter source pos: ");
+    }
+
+    @Override
+    public Position getSourcePiecePosition(Board board, Piece.color playerColor) {
+        Scanner scanner = new Scanner(System.in);
+        String sourcePos = scanner.nextLine();
+
+        int sourceRow = sourcePos.charAt(0) - 'a';
+        int sourceCol = sourcePos.charAt(1) - '1';
+
+        return new Position(sourceRow, sourceCol);
+    }
+
+    @Override
+    public void askWhereToMove(Board board, Piece.color playerColor, ArrayList<Move> validMoves) {
+        for(Move move: validMoves) {
+            displayMove(move);
+        }
+
+        System.out.println("Enter dest pos: ");
+    }
+
+    @Override
+    public Position getDestinationPosition(Board board, Piece.color playerColor) {
+        Scanner scanner = new Scanner(System.in);
+        String destPos = scanner.nextLine();
+
+        int destRow = destPos.charAt(0) - 'a';
+        int destCol = destPos.charAt(1) - '1';
+
+        return new Position(destRow, destCol);
     }
 }
