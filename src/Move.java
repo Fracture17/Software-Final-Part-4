b7108b610a3c3d34b36039660de11be55d3d9ca8
john <johnotters@yahoo.com>
2019-12-10 18:00:06 -0800

    Implements Move data structure

---
+++ src/Move.java
@@ -0,0 +1,13 @@
+public class Move {
+    public Move(int sourceRow, int sourceCol, int destRow, int destCol) {
+        this.sourceRow = sourceRow;
+        this.sourceCol = sourceCol;
+        this.destRow = destRow;
+        this.destCol = destCol;
+    }
+
+    public int sourceRow;
+    public int sourceCol;
+    public int destRow;
+    public int destCol;
+}
